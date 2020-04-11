edbb782c8aa7d90c16c9b6e063110117eec913bb
AnaLul <luhigalgo01@gmail.com>
2020-04-11 00:12:11 -0600

    Update PersonRecord.tsx

--- src/person-record/PersonRecord.tsx
+++ src/person-record/PersonRecord.tsx
@@ -1,13 +1,21 @@
 import { createUseStyles } from "react-jss";
 import React, { useState } from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, Select, MenuItem, FilledInput, InputAdornment, Input } from "@material-ui/core";
 import { CustomTextField } from "../shared/components/TextField";
 
 interface IPersonState {
   name: string;
   lastName: string;
   email: string;
+  objective: string;
+  height: number;
 }
+
+interface IObjective {
+  name: string;
+}
+
+const objectives: Array<IObjective> = [{ name: 'Tren superior' }, { name: 'Tren inferior' }, { name: 'Cardio' }];
 
 const useStyles = createUseStyles({
   container: {
@@ -20,6 +28,8 @@ export const PersonRecord: React.FunctionComponent = () => {
     name: '',
     lastName: '',
     email: '',
+    objective: '',
+    height: 0,
   });
 
   const onPropChange = (propName: string) => (event: any) => {
@@ -28,6 +38,7 @@ export const PersonRecord: React.FunctionComponent = () => {
       [propName]: event.target.value,
     });
   }
+
   const classes = useStyles();
   return (
     <>
@@ -44,6 +55,39 @@ export const PersonRecord: React.FunctionComponent = () => {
       <Grid container className={classes.container}>
         <Grid item xs={6}>
           <CustomTextField label="Correo" value={state.email} onChange={onPropChange('email')} placeholder="sample@mail.com"></CustomTextField>
+        </Grid>
+      </Grid>
+      <Grid container className={classes.container}>
+        <Grid item xs={6} >
+          <Select
+            labelId="demo-simple-select-placeholder-label-label"
+            id="demo-simple-select-placeholder-label"
+            value={state.objective}
+            onChange={onPropChange('objective')}
+            inputProps={{ 'aria-label': 'Without label' }}
+            displayEmpty
+          >
+            <MenuItem value="" disabled>
+              Seleccione un objetivo
+            </MenuItem>
+            {
+              objectives.map((objective) => {
+                return <MenuItem value={objective.name}>{objective.name}</MenuItem>
+              })
+            }
+          </Select>
+        </Grid>
+      </Grid>
+      <Grid container className={classes.container}>
+        <Grid item xs={6}>
+        <Input
+            type="Number"
+            inputProps={{className:'digitsOnly', step: "0.1"}}
+            value={state.height}
+            onChange={onPropChange('height')}
+            endAdornment={<InputAdornment position="end">m</InputAdornment>}
+            aria-describedby="filled-height-helper-text"
+          />
         </Grid>
       </Grid>
     </>
